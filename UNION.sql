2a08060f8b6885090e7cf8b13d9558c38fc5f2a2
aaditya6882 <aadityaacharya6882@gmail.com>
2025-07-09 14:18:29 +0545

    Create UNION.sql

---
+++ UNION.sql
@@ -0,0 +1,29 @@
+SELECT first_name, last_name
+FROM employee_demographics
+UNION DISTINCT
+SELECT first_name, last_name
+FROM employee_salary
+;
+
+SELECT first_name, last_name
+FROM employee_demographics
+UNION ALL
+SELECT first_name, last_name
+FROM employee_salary
+;
+
+
+
+SELECT first_name, last_name, 'old Male' AS Label
+FROM employee_demographics
+WHERE age>40 AND gender='Male'
+UNION
+SELECT first_name, last_name, 'old Lady' AS Label
+FROM employee_demographics
+WHERE age>40 AND gender='Female'
+UNION
+SELECT first_name, last_name, 'high paid' AS Label
+FROM employee_salary
+WHERE salary>70000
+ORDER BY first_name, last_name
+;
